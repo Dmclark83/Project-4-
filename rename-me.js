32c1765407e543a80a2bcdf202bc2913de11d624
Darius Clark <dariusclark73@gmail.com>
2011-08-23 10:43:29 -0500

    8/22/11 I think I figured out how to save to git

---
+++ rename-me.js
@@ -0,0 +1,17 @@
+alert("JavaScript works!");
+							
+							
+var getSales = function calc_sales(units_a, units_b, units_c) {
+   return units_a*79 + units_b * 129 + units_c * 699;
+  };
+ // console.log(getTime(83,94,44))
+  
+ // 
+var getSmallestvalue = function (a, b){
+	
+  return a - b;
+
+
+}
+var cost= [10,43,6,7,83,2];
+console.log(getSmallestvalue(cost))
